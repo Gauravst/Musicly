5fc169d6ac74dc60ee417531ff2c970eb0187ea5
Gaurav Singh Tangariya <gauravsinghtangariya1@gmail.com>
2023-06-16 02:36:03 +0530

    New Update

---
+++ README.md
@@ -0,0 +1,55 @@
+## Musicly
+
+Musicly is a music app that brings the joy of music to your life.
+
+### Description
+
+Musicly is a web-based music application that allows users to listen to their favorite songs, discover new music, create playlists, and enjoy a seamless music experience. With a user-friendly interface and a vast library of songs, Musicly is designed to enhance your music listening journey.
+
+### Features
+
+- Browse and search for songs
+- Create and manage playlists
+- Play, pause, skip, and repeat songs
+- Discover recommended songs based on your preferences
+- Personalize your profile and music preferences
+- Responsive design for a seamless experience across devices
+
+### Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+### Installation
+
+1. Clone the repository:
+git clone https://github.com/Gauravst/Musicly.git
+
+2. Open the project directory:
+
+
+3. Launch the app:
+
+- Open `index.html` in your preferred web browser.
+
+### Usage
+
+- Browse and search for songs using the search bar.
+- Click on a song to start playing it.
+- Create playlists and add songs to them.
+- Customize your profile settings.
+- Explore recommended songs based on your listening history.
+
+### Credits
+
+<!-- - [Open-source music API](https://example-music-api.com) - Provides access to a vast music library.
+- [Icons8](https://icons8.com) - Icons used in the app. -->
+
+### License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more information.
+
+### Acknowledgements
+
+We would like to thank our team members and contributors for their valuable contributions to this project.
--- js/script.js
+++ js/script.js
@@ -1,162 +1,56 @@
 // Initialize the Variables
 let song_id = 1;
-let audio_Element = new Audio('songs/song1.mp3');
 let start_time;
 let current_time;
 let audio_time;
-let bar = document.getElementById('progress_bar');
-let play = document.getElementById('play');
-let back = document.getElementById('back');
-let next = document.getElementById('next');
-let song_items = Array.from(document.getElementsByClassName('song_x1'));
-// let playr = Array.from(document.getElementsByClassName('playr'));
-// console.log(playr);
+let bar = document.getElementById("progress_bar");
+let play = document.getElementById("play");
+let back = document.getElementById("back");
+let next = document.getElementById("next");
+let forYouList = document.getElementById("forYouList");
+let trending = document.getElementById("trending");
+let topList = document.getElementById("topList");
 
-console.log(song_items);
+const AddSongs = (songs, item, where) => {
+  for (let i = 0; i < item; i++) {
+    const song = songs[i];
 
-// Songs Database
-let songs = [
-  {
-    song_id: 1,
-    song_name: "HAAD MASALA - GULZAAR CHHANIWALA",
-    song_url: "https://t.ly/-ATEM",
-    song_cover: "https://t.ly/NrDs",
-    song_time: "03:28"
-  },
+    const songDiv = document.createElement("div");
+    songDiv.className = "songItem";
 
-  {
-    song_id: 2,
-    song_name: "Besharam Rang - Phathan",
-    song_url: "https://t.ly/-0VZ",
-    song_cover: "https://t.ly/jCuu",
-    song_time: "03:14"
-  },
+    const title = (name) => {
+      if (name.length < 25) {
+        return name;
+      } else {
+        return name.substr(0, 22) + "...";
+      }
+    };
 
-  {
-    song_id: 3,
-    song_name: "Tu Te Sharab - Jordan Sandhu",
-    song_url: "#",
-    song_cover: "https://rb.gy/9c46uo",
-    song_time: "03:14"
-  },
+    songDiv.innerHTML = `
+    <div class = "ItemLeft">
+      <img src=${song.song_cover || "../assets/img/cover.png"} class="song_cover_x1" />
+      <div class="song_title_x1">${title(song.song_name)}</div>
+    </div>
+    <div class="ItemRight">
+      <div class="song_time">${song.song_time}</div>
+      <i id="1" class="playr fa-regular fa-circle-play"></i>
+    </div>
+    `;
 
-  {
-    song_id: 4,
-    song_name: "Bling Bling - Dilnoor",
-    song_url: "https://t.ly/jD1x",
-    song_cover: "https://rb.gy/kjlxj6",
-    song_time: "03:14"
-  },
+    where.appendChild(songDiv);
+  }
+};
 
-  {
-    song_id: 5,
-    song_name: "Main Khiladi - Udit Narayan",
-    song_url: "https://rb.gy/tv4nej",
-    song_cover: "https://rb.gy/ryiv1x",
-    song_time: "03:07"
-  },
+// Fetching data
+fetch("../db/songs.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const songs = data;
 
-  {
-    song_id: 6,
-    song_name: "Chedkhaniyan - Arijit Singh, Nikhita Gandhi",
-    song_url: "https://rb.gy/rfsvwb",
-    song_cover: "https://rb.gy/9c46uo",
-    song_time: "03:15"
-  },
-
-  {
-    song_id: 7,
-    song_name: "Kalaiyaan - Mani Cheema",
-    song_url: "https://rb.gy/9iyzc5",
-    song_cover: "http://t.ly/Ei66",
-    song_time: "03:14"
-  },
-
-  {
-    song_id: 8,
-    song_name: "Bling Bling - Dilnoor",
-    song_url: "https://t.ly/jD1x",
-    song_cover: "https://rb.gy/kjlxj6",
-    song_time: "02:53"
-  },
-
-  {
-    song_id: 9,
-    song_name: "Jhanjran -  Babbu Maan",
-    song_url: "http://shorturl.at/itIQW",
-    song_cover: "http://shorturl.at/goqt7",
-    song_time: "03:14"
-  },
-
-  {
-    song_id: 10,
-    song_name: "Main Khiladi - Udit Narayan",
-    song_url: "https://rb.gy/tv4nej",
-    song_cover: "https://rb.gy/ryiv1x",
-    song_time: "03:07"
-  },
-
-  {
-    song_id: 11,
-    song_name: "HAAD MASALA - GULZAAR CHHANIWALA",
-    song_url: "https://t.ly/-ATEM",
-    song_cover: "https://t.ly/NrDs",
-    song_time: "03:28"
-  },
-
-  {
-    song_id: 12,
-    song_name: "Bling Bling - Dilnoor",
-    song_url: "https://t.ly/jD1x",
-    song_cover: "https://rb.gy/kjlxj6",
-    song_time: "02:53"
-  },
-]
-
-Array.from(document.getElementsByClassName('playr')).forEach((element) => {
-  element.addEventListener('click', (e) => {
-    if (audio_Element.paused || audio_Element.currentTime <= 0) {
-      audio_Element.src = 'songs/song1.mp3';
-      audio_Element.play();
-      audio_Element.currentTime = 0;
-      play.classList.remove('fa-circle-play');
-      play.classList.add('fa-circle-pause');
-      document.getElementsByClassName('playr').classList.remove('fa-circle-play');
-      document.getElementsByClassName('playr').classList.add('fa-circle-pause');
-    }
-    else {
-      audio_Element.pause();
-      play.classList.add('fa-circle-play');
-      play.classList.remove('fa-circle-pause');
-      document.getElementsByClassName('playr').classList.add('fa-circle-play');
-      document.getElementsByClassName('playr').classList.remove('fa-circle-pause');
-    }
+    AddSongs(songs.forYouDB, 6, forYouList);
+    AddSongs(songs.trendingDB, 2, trending);
+    AddSongs(songs.topDB, 3, topList);
   })
-});
-
-console.log("check1");
-// console.log(song);
-// adding data to song list
-song_items.forEach((element, i) => {
-  element.getElementsByClassName("song_title_x1")[0].innerHTML = songs[i].song_name;
-  element.getElementsByClassName("song_cover_x1")[0].src = songs[i].song_cover;
-  element.getElementsByClassName("song_time")[0].innerHTML = songs[i].song_time;
-});
-
-console.log("check2")
-
-// // play pause by main button
-// playy.addEventListener('click', () =>{
-//   if(audio_Element.paused || audio_Element.currentTime<=0){
-//     audio_Element.play();
-//     playy.classList.remove('fa-circle-play');
-//     playy.classList.add('fa-circle-pause');
-//   } else {
-//     audio_Element.pause();
-//     playy.classList.remove('fa-circle-pause');
-//     playy.classList.add('fa-circle-play');
-//   }
-// })
-
-console.log("hello")
-
+  .catch((error) => {
+    console.error("Error loading song database:", error);
+  });
